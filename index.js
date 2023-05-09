0479a4924a156573ef3a4fb5bf2fd9b6b666ebca
ian <116136272+en-ian-0131@users.noreply.github.com>
2023-05-09 17:25:02 +0800

    login-logout

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express');
-
 const server = express();
 const db = require('./module/db-connect');
 const multer = require('multer');
+const session = require('express-session')
+const bcrypt = require('bcryptjs');
 
 server.set('view engine','ejs');
 
+//middleware
+server.use(express.urlencoded({extended:false}))
+server.use(session({
+    saveUninitialized:false,
+    resave:false,
+    secret:'nlrjviljfljnkjmsdo623gb3g3n2t32b',
+    cookie:{
+        maxAge:120_000
+    }
+}))
+server.use((req,res,next)=>{
+    res.locals.session=req.session;
+    next();
+})
+
+
+//route
 server.get('/',(req,res)=>{
     // res.send(`<h1>Hello world~ </h1>`)
-    res.render('main',{name:'請先登入~'});
+    if(!req.session.admin){
+         res.render('main',{name:'請先登入~',title:''});
+    }else{
+        res.render('main',{name:`${req.session.admin.account}`,title:'歡迎回來~'});
+    }
+   
 })
 
 server.get('/try-db',async (req,res)=>{
@@ -17,10 +40,17 @@ server.get('/try-db',async (req,res)=>{
     res.json(rows);
 })
 
-const urlencoded = express.urlencoded({extended:false});
-server.post('/try-post',urlencoded,(req,res)=>{
+// const urlencoded = express.urlencoded({extended:false});
+server.post('/try-post',(req,res)=>{
     console.log({body:req.body})
     res.json({body:req.body});
+})
+
+
+server.get('/try-sess',(req,res)=>{
+    req.session.mysess = req.session.mysess || 0;
+    req.session.mysess++;
+    res.send(req.session)
 })
 
 
@@ -30,9 +60,38 @@ server.get('/login',(req,res)=>{
 
 
 //use URLSearchParams to parse application/x-www-form-urlencoded
-server.post('/login',urlencoded,(req,res)=>{
-    console.log(req.body);
-    res.json(req.body);
+server.post('/login',async (req,res)=>{
+//    console.log(req.body);
+   
+   output={
+    success:false,
+    code:0,
+    postData:req.body,
+    error:'帳號密碼錯誤!!'
+   }
+   
+   const sql = 'SELECT * FROM admins WHERE account=?';
+   const [rows] = await db.query(sql,[req.body.account]);
+   if(!rows.length){
+    output.code = 401;
+    return res.json(output);
+   }
+//    console.log(rows);
+   if(!await bcrypt.compare(req.body.password,rows[0].password_hash)){
+    output.code=402;
+    output.error='密碼錯誤!!'
+    res.json(output);
+   }else{
+    output.code=200;
+    output.success=true;
+    output.error='';
+    req.session.admin = {
+        sid:rows[0].sid,
+        account:rows[0].account
+    }
+    res.json(output);
+   }
+  
 })
 
 //use multer to parse multipart/form-data
@@ -42,7 +101,8 @@ server.post('/loginFormData',BP.none(),(req,res)=>{
     res.json(req.body);
 })
 server.get('/logout',(req,res)=>{
-    
+    delete req.session.admin;
+    res.redirect('/');
 })
 
 server.use(express.static('public'))
